29471f66f990b4583c6bddab50adfd85f4313b7e
Mateusz <matthew.kilan@gmail.com>
2014-07-30 12:05:45 +0200

    Added static picture superstarbadge, deleted paperclip superstarbadge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
   
     def configure_devise_permitted_parameters
-        registration_params = [:name, :email, :password, :password_confirmation,:avatar,:superstarbadge]
+        registration_params = [:name, :email, :password, :password_confirmation,:avatar]
         
         if params[:action] == "update"
           devise_parameter_sanitizer.for(:account_update) {
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :omniauthable
   # :recoverable, :rememberable and :trackable
   devise :database_authenticatable, :registerable, :validatable
-  has_attached_file :avatar, :styles => { :small => "100x100>" }, :default_url => "/images/one.jpg"
+  has_attached_file :avatar, :styles => { :small => "100x100>" }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
-  has_attached_file :superstarbadge, :styles => { :small => "100x100>" }, :default_url => "/images/superbadge.jpeg" 
-  validates_attachment_content_type :superstarbadge, :content_type => /\Aimage\/.*\Z/
+  # has_attached_file :superstarbadge, :styles => { :small => "100x100>" } 
+  # validates_attachment_content_type :superstarbadge, :content_type => /\Aimage\/.*\Z/
 
   has_many :questions
   has_many :answers

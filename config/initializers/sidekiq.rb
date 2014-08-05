fd7c31244aaf4bc23314980c0babaedc309fe139
Mateusz <matthew.kilan@gmail.com>
2014-08-05 12:12:21 +0200

    Heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://redis.example.com:9547/12', :namespace => 'mynamespace' }
+  config.redis = { :url => 'redis://redistogo:308170c9df9214c5ec94150a1155477b@hoki.redistogo.com:9547/', :namespace => 'mynamespace' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => 'redis://redis.example.com:9547/12', :namespace => 'mynamespace' }
+  config.redis = { :url => 'redis://redistogo:308170c9df9214c5ec94150a1155477b@hoki.redistogo.com:9547/', :namespace => 'mynamespace' }
 end

5b414f5bb2c06b50fb256fa29dc0ff342e5bcd88
Mateusz <matthew.kilan@gmail.com>
2014-08-05 14:03:48 +0200

    Heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -18,7 +18,7 @@ end
 after_fork do |server, worker|
   
   Sidekiq.configure_server do |config|
-    config.redis = { :url => ENV["REDISTOGO_URL"], :namespace => 'mynamespace', :size => 1 }
+    config.redis = { :url => ENV["REDISTOGO_URL"], :namespace => 'mynamespace', :size => 5 }
   end
   
   Sidekiq.configure_client do |config|

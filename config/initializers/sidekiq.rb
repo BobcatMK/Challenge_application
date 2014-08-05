91890d83cfc8d88f96f1fdb0a75626aed2ed6de5
Mateusz <matthew.kilan@gmail.com>
2014-08-05 12:45:48 +0200

    Heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => ENV["REDISTOGO_URL"], :namespace => 'mynamespace' }
+  config.redis = { :url => ENV["REDISTOGO_URL"], :namespace => 'mynamespace', :size => 1 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => ENV["REDISTOGO_URL"], :namespace => 'mynamespace' }
+  config.redis = { :url => ENV["REDISTOGO_URL"], :namespace => 'mynamespace', :size => 5 }
 end
